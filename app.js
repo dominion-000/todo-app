96b1a19fe9e488ef0c7a4f89e611331bc1bb40aa
Dominionthedeveloper <Dominionthedeveloper@gmail.com>
2026-04-02 00:20:44 +0100

    feat(app): put all components together and solidify the app

---
+++ app.js
@@ -0,0 +1,66 @@
+const express = require("express");
+const app = express();
+
+const mongoose = require("mongoose");
+const User = require("./models/User");
+const Task = require("./models/Task");
+const session = require("express-session");
+const authRoutes = require("./routes/auth");
+const taskRoutes = require("./routes/tasks");
+
+// allow sessions
+app.use(session({
+  secret: "secret-key",
+  resave: false,
+  saveUninitialized: false
+}));
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+// routes
+app.get("/", (req, res) => {
+  res.send("Todo App Running 🚀");
+});
+
+// auth
+app.use("/auth", authRoutes);
+
+// tasks
+app.use("/tasks", taskRoutes);
+
+// test db
+app.get("/test-db", async (req, res) => {
+  try {
+    const testCollection = mongoose.connection.db.collection("test");
+    await testCollection.insertOne({ message: "DB works 🚀" });
+
+    res.send("Inserted into DB ✅");
+  } catch (err) {
+    console.error(err);
+    res.status(500).send("DB test failed ❌");
+  }
+});
+
+// test models
+app.get("/test-models", async (req, res) => {
+  try {
+    const user = await User.create({
+      username: "testuser",
+      password: "123456"
+    });
+
+    const task = await Task.create({
+      title: "My first task",
+      userId: user._id
+    });
+
+    res.json({ user, task });
+  } catch (err) {
+    console.error(err);
+    res.status(500).send("Model test failed ❌");
+  }
+});
+
+// export
+module.exports = app;
